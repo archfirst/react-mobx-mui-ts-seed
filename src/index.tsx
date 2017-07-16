a201e36ef1377dbd0f7f2ac27370fa6b82b1a3e7
Naresh Bhatia <naresh.archfirst@gmail.com>
2017-07-16 10:27:26 -0400

    Enabled strict mode for MobX

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { useStrict } from 'mobx';
 import App from './app';
 import registerServiceWorker from './register-service-worker';
+
+// Enable strict mode for MobX. This disallows state changes outside of an action
+useStrict(true);
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
